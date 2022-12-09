83623f0568db778b28ec2393e8bce88885311cde
eulbyvan <work.issdq@gmail.com>
2022-12-09 23:56:19 +0700

    add GetProductById api

--- src/main/java/com/eulbyvan/controller/ProductController.java
+++ src/main/java/com/eulbyvan/controller/ProductController.java
@@ -4,10 +4,12 @@ import com.eulbyvan.model.Product;
 import com.eulbyvan.service.IProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author stu (https://www.eulbyvan.com/)
@@ -27,6 +29,13 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<List<Product>> getProducts() {
         List<Product> products = productService.getProducts();
+
+        return ResponseEntity.ok().body(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Product>> getProductById(@PathVariable("id") Long id) {
+        Optional<Product> products = productService.getProductById(id);
 
         return ResponseEntity.ok().body(products);
     }
