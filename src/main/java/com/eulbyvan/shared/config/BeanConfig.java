b440ca25a8ede5576474a18b3ce1e31a19c9b698
eulbyvan <work.issdq@gmail.com>
2022-12-09 22:20:46 +0700

    Update BeanConfig.java

--- src/main/java/com/eulbyvan/shared/config/BeanConfig.java
+++ src/main/java/com/eulbyvan/shared/config/BeanConfig.java
@@ -1,4 +1,4 @@
-package com.eulbyvan.security;
+package com.eulbyvan.shared.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
