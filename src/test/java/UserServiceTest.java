853838471df1ad71f1df949a7f1a38755ab0ba82
eulbyvan <work.issdq@gmail.com>
2022-12-09 13:48:13 +0700

    Create UserServiceTest.java

---
+++ src/test/java/UserServiceTest.java
@@ -0,0 +1,45 @@
+import com.eulbyvan.model.User;
+import com.eulbyvan.repo.IRoleRepo;
+import com.eulbyvan.repo.IUserRepo;
+import com.eulbyvan.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author stu (https://www.eulbyvan.com/)
+ * @version 1.0
+ * @since 09/12/2022
+ */
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+    @Mock
+    private IUserRepo mockUserRepo;
+    @Mock
+    private IRoleRepo mockRoleRepo;
+    private UserService userService;
+
+    @BeforeEach
+    void setup() {
+        userService = new UserService(mockUserRepo, mockRoleRepo);
+    }
+
+    @Test
+    public void itShould_ReturnListOfUser_When_GetUsers() {
+        List<User> dummyUsers = new ArrayList<>();
+        when(mockUserRepo.findAll()).thenReturn(dummyUsers);
+
+        List<User> actualUsers = userService.getUsers();
+
+        assertEquals(actualUsers, dummyUsers);
+    }
+}
