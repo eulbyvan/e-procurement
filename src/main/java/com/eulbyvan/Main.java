19a592cdbf2af40fd5ba79354ebd4143f1405688
eulbyvan <work.issdq@gmail.com>
2022-12-09 09:31:55 +0700

    configure db

--- src/main/java/com/eulbyvan/Main.java
+++ src/main/java/com/eulbyvan/Main.java
@@ -1,13 +1,17 @@
 package com.eulbyvan;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author stu (https://www.eulbyvan.com/)
  * @version 1.0
- * @since ${DATE}
+ * @since 09/12/2022
  */
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
