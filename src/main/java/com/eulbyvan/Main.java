f4d873c7b6d1d6915e0aa574541b1cea49904856
eulbyvan <work.issdq@gmail.com>
2022-12-09 08:57:52 +0700

    initial commit

---
+++ src/main/java/com/eulbyvan/Main.java
@@ -0,0 +1,13 @@
+package com.eulbyvan;
+
+/**
+ * @author stu (https://www.eulbyvan.com/)
+ * @version 1.0
+ * @since ${DATE}
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
