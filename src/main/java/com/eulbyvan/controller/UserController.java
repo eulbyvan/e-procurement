ed13b1c5e92d57904fb17741eceb9b0a384742d0
eulbyvan <work.issdq@gmail.com>
2022-12-09 14:44:20 +0700

    add save user, role, and assign role

--- src/main/java/com/eulbyvan/controller/UserController.java
+++ src/main/java/com/eulbyvan/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.eulbyvan.controller;
 
+import com.eulbyvan.model.Role;
 import com.eulbyvan.model.User;
+import com.eulbyvan.model.req.UserRole;
 import com.eulbyvan.service.IUserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 /**
@@ -31,5 +32,23 @@ public class UserController {
         List<User> users = userService.getUsers();
 
         return ResponseEntity.ok().body(users);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> saveUser(@RequestBody User req) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/users").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(req));
+    }
+
+    @PostMapping("/roles")
+    public ResponseEntity<Role> saveRole(@RequestBody Role req) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/roles").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(req));
+    }
+
+    @PostMapping("/user/roles")
+    public ResponseEntity<?> assignRole(@RequestBody UserRole req) {
+        userService.assignRole(req.getUsername(), req.getRoleName());
+        return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/com/eulbyvan/model/req/UserRole.java
@@ -0,0 +1,17 @@
+package com.eulbyvan.model.req;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author stu (https://www.eulbyvan.com/)
+ * @version 1.0
+ * @since 09/12/2022
+ */
+
+@Data
+public class UserRole {
+    private String username;
+    private String roleName;
+}
